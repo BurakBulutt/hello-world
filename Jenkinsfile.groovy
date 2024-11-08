08b71cc43e612d4a80afb24359d4c5c278fcb534
burak-bulut <lotheas01@outlook.com>
2024-11-08 04:28:44 +0300

    init commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,42 @@
+pipeline {
+    agent any
+    environment {
+        WORKDIR = '/home/ubuntu/hello-world'
+    }
+    tools {
+        maven 'maven3.9.9'
+    }
+    stages {
+        stage('Pull Latest') {
+            steps {
+                dir(WORKDIR) {
+                    sh 'git fetch'
+                    sh 'git checkout dev'
+                    sh 'git pull'
+                }
+            }
+        }
+        stage('Build with Maven') {
+            steps {
+                dir(WORKDIR) {
+                    sh 'mvn clean package'
+                }
+            }
+        }
+        stage('Deploy with Docker') {
+            steps {
+                dir(WORKDIR) {
+                    sh 'bash deploy.sh'
+                }
+            }
+        }
+    }
+    post {
+        success {
+            echo 'Deployment completed.'
+        }
+        failure {
+            echo 'Deployment failed.'
+        }
+    }
+}
